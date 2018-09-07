50dd74d145a360adf0c2bd50b1df3f26aa72b615
harnoorsinghdev <36705841+harnoorsinghdev@users.noreply.github.com>
2018-09-07 13:48:55 +0530

    Updation of functions

--- Students_comparision.py
+++ Students_comparision.py
@@ -1,6 +1,5 @@
 
 
-
 dictOFMARKS={200:('Name 1','Section a','2018','Python'),100:('Name 2','Section B','2019','English'),80:('Name 3','Section a','2018','Python'),90:('Name 4','Section B','2017','Python')}
 lenn=len(dictOFMARKS)
 def sum():
@@ -12,5 +11,25 @@ def sum():
         if(i==lenn):
             return sum1
         
+def avg():
+    avg1=0
+    
+    for k in dictOFMARKS.keys():
+        sum1=sum()
+        return sum1/lenn
+        
+def high():
+    high=0
+    for k in dictOFMARKS.keys():
+        if(k>high):
+            high=k
+        return high    
+        
 out=sum()
-print(out)
+print("sum is",out)
+
+avgg=avg()
+print("average is",avgg)
+        
+highest=high()
+print("highest is",highest)
